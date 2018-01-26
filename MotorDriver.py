589466484ccb06c965a03fc01b38c0cf6602f03b
thiagobach <35727474+thiagobach@users.noreply.github.com>
2018-01-26 22:08:18 +0900

    Update MotorDriver.py

--- MotorDriver.py
+++ MotorDriver.py
@@ -1,51 +1,42 @@
 import RPi.GPIO as GPIO # import GPIO librery
 from time import sleep
 
+class motor():
+    
+    def __init__(self,Input1Pin,Input2Pin,EnablePin):
+        self.IAp = Input1Pin
+        self.IBp = Input2Pin
+        self.ENp = EnablePin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.IAp,GPIO.OUT)
+        GPIO.setup(self.IBp,GPIO.OUT)
+        GPIO.setup(self.ENp,GPIO.OUT)
+        self.pwm=GPIO.PWM(self.ENp,100)
+        self.pwm.start(50) # starting it with 50% dutycycle
+        #Motor1A = 24 # set GPIO-02 as Input 1 of the controller IC
+        #Motor1B = 23 # set GPIO-03 as Input 2 of the controller IC 
+        #Motor1E = 25 # set GPIO-04 as Enable pin 1 of the controller IC
+        return ("This motor is ready to rock and roll")
 
-#Setup the GPIO
-GPIO.setmode(GPIO.BCM)
-Motor1A = 24 # set GPIO-02 as Input 1 of the controller IC
-Motor1B = 23 # set GPIO-03 as Input 2 of the controller IC 
-Motor1E = 25 # set GPIO-04 as Enable pin 1 of the controller IC
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
-pwm=GPIO.PWM(Motor1E,100) # configuring Enable pin means GPIO-04 for PWM
-pwm.start(50) # starting it with 50% dutycycle
-
-def runMotor(Speed,forward):
-    pwm.ChangeDutyCycle(Speed)
-    GPIO.output(Motor1E,GPIO.HIGH)
-
-    if forward:
-        GPIO.output(Motor1B,GPIO.LOW)
-        GPIO.output(Motor1A,GPIO.HIGH)
-    else:
-        GPIO.output(Motor1B,GPIO.HIGH)
-        GPIO.output(Motor1A,GPIO.LOW)
-        
+        def runMotor(Speed=50,forward=True):
+            self.pwm.ChangeDutyCycle(Speed)
+            GPIO.output(self.ENp,GPIO.HIGH)
+            if forward:
+                GPIO.output(self.IBp,GPIO.LOW)
+                GPIO.output(self.IAp,GPIO.HIGH)
+                return ("Motor is running forward")
+            else:
+                GPIO.output(self.IBp,GPIO.HIGH)
+                GPIO.output(self.IAp,GPIO.LOW)
+                return ("Motor is running backwards")
     
     
-def stop():
-    GPIO.output(Motor1E,GPIO.LOW)
+        def stop():
+            GPIO.output(self.ENp,GPIO.LOW)
+            return("Motor stopped")
 
-def turnOff():
-    pwm.stop() # stop PWM from GPIO output it is necessary
-    GPIO.cleanup()
-    print("Off")
+        def close():
+            self.pwm.stop() # stop PWM from GPIO output it is necessary
+            GPIO.cleanup()
+            return ("Motors are turned Off")
 
-runMotor(100,False)
-sleep(3)
-
-runMotor(50,False)
-sleep(3)
-
-runMotor(30,False)
-sleep(3)
-
-runMotor(100,True)
-sleep(3)
-
-
-stop()
-turnOff()
